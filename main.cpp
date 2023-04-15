0a4e4c868126134447755819cd28b51000d5d370
Brendan Chen <smallpigeon1@gmail.com>
2023-04-14 18:21:14 -0700

    make some linked lists

--- main.cpp
+++ main.cpp
@@ -1,19 +1,30 @@
 #include <iostream>
 #include "Node.hpp"
+#include "LinkedList.hpp"
+
+using namespace std;
 
 int main() {
   int arr[5] = {5, 9, 15, 61, -5};
   int len = sizeof(arr) / sizeof(arr[0]);
 
-  Node* start = new Node(arr[0]);
-  Node* oldNode = start;
+  // Node* start = new Node(arr[0]);
+  // Node* oldNode = start;
 
-  for (int i = 1; i < len; i++) {
-    Node* newNode = new Node(arr[i]);
-    oldNode->setNext(newNode);
-    oldNode = newNode;
+  // for (int i = 1; i < len; i++) {
+  //   Node* newNode = new Node(arr[i]);
+  //   oldNode->setNext(newNode);
+  //   oldNode = newNode;
+  // }
+
+  // delete start;
+
+  LinkedList* list = new LinkedList();
+  for (int i = 0; i < len; i++) {
+    list->append(arr[i]);
   }
 
-  delete start;
+  std::cout << list->get(2)->getData() << std::endl;
+
   return 0;
 }
